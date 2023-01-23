894a9370aee525c42140a373f5248d2e831736dd
Corey Shaw <coreys@outlook.com>
2023-01-22 19:14:22 -0800

    Add missing dependency, fix issues running python script

--- scripts/init-schema.sh
+++ scripts/init-schema.sh
@@ -1,2 +1,3 @@
-docker exec citadel-db psql -f delta_data_schema.sql
-docker exec citadel-db psql -f delta_taxa_schema.sql
+docker exec citadel-db psql -c "CREATE DATABASE blaseball;"
+docker exec citadel-db psql -f delta_data_schema.sql -d blaseball
+docker exec citadel-db psql -f delta_taxa_schema.sql -d blaseball
--- scripts/init-test-env.sh
+++ scripts/init-test-env.sh
@@ -1,5 +1,6 @@
 docker stop citadel-db
+docker network create citadel-network
 docker build -t sibr/citadel-db:latest -f .\docker\db\Dockerfile .
 docker build -t sibr/citadel-app:latest -f .\docker\app\Dockerfile .
-docker run --rm -d --name citadel-db -e POSTGRES_PASSWORD=testpassword sibr/citadel-db:latest
+docker run --rm -d --name citadel-db -e POSTGRES_PASSWORD=testpassword -e POSTGRESS_DATABASE=blaseball --net citadel-network sibr/citadel-db:latest
 echo "Initialization complete."
